d9258d56d4f2f8e19506a764dd2713dfdac5d964
student <student@apc02hm0z4dv7n-2.local>
2018-04-10 19:06:18 -0400

    Initial Commit

---
+++ StudyBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyBuddy
+//
+//  Created by student on 4/10/18.
+//  Copyright © 2018 cs@eku.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
