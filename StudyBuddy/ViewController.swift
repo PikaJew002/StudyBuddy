642979d871f1d7caa59afdf99c83fb071991d333
student <student@apc02hm0z4dv7n-7.local>
2018-04-26 16:29:08 -0400

    URL building is finishing, JSON data conversion is still in progress

--- StudyBuddy/ViewController.swift
+++ StudyBuddy/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 struct User: Codable {
     var email: String = ""
-    var username: String = ""
-    var password: String = ""
     var first_name: String = ""
     var last_name: String = ""
+    var password: String = ""
+    var username: String = ""
 }
 
 class ViewController: UIViewController {
@@ -27,16 +27,29 @@ class ViewController: UIViewController {
         if !username.text!.isEmpty && !password.text!.isEmpty {
             // select * from user where email = $email
             let id = "21232f297a57a5a743894a0e4a801fc3"
-            let urlStr = "http://baruchhaba.org/StudyBuddy/query.php?id=\(id)&type=select&table=user&condition=email=\(username.text!)"
-            URLSession.shared.dataTask(with: URL(string: urlStr)!, completionHandler: { (data, response, error) in
-                do {
-                    let decoder = JSONDecoder()
-                    self.user = try decoder.decode(User.self, from: data!)
-                } catch {
-                    
+            let urlStr = "http://baruchhaba.org/StudyBuddy/query.php?id=\(id)&type=select&table=user&condition=email%20=%20%22\(username.text!)%22"
+            print(urlStr)
+            if let url = URL(string: urlStr) {
+                URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                    do {
+                        let decoder = JSONDecoder()
+                        if data != nil {
+                            self.user = try decoder.decode(User.self, from: data!)
+                        } else {
+                            print("data is nil")
+                        }
+                    } catch {
+                        
+                    }
+                }).resume()
+                if !user.email.isEmpty {
+                    print(user.email)
+                } else {
+                    print("no email")
                 }
-            }).resume()
-            
+            } else {
+                print("url is nil")
+            }
         }
     }
     
