3dbcd647e5870fc45615c3f85261b6ebbf69ab42
student <student@apc02hm0z4dv7n-7.local>
2018-04-26 17:01:20 -0400

    JSON data retrieval is working, validation before segue is not

--- StudyBuddy/ViewController.swift
+++ StudyBuddy/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 struct User: Codable {
-    var email: String = ""
-    var first_name: String = ""
-    var last_name: String = ""
-    var password: String = ""
-    var username: String = ""
+    var email: String
+    var first_name: String
+    var last_name: String
+    var password: String
+    var username: String
 }
 
 class ViewController: UIViewController {
@@ -21,35 +21,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     
-    var user: User = User()
+    var user: User = User(email: "", first_name: "", last_name: "", password: "", username: "")
     
     @IBAction func login(_ sender: UIButton) {
         if !username.text!.isEmpty && !password.text!.isEmpty {
             // select * from user where email = $email
-            let id = "21232f297a57a5a743894a0e4a801fc3"
-            let urlStr = "http://baruchhaba.org/StudyBuddy/query.php?id=\(id)&type=select&table=user&condition=email%20=%20%22\(username.text!)%22"
-            print(urlStr)
-            if let url = URL(string: urlStr) {
-                URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-                    do {
-                        let decoder = JSONDecoder()
-                        if data != nil {
-                            self.user = try decoder.decode(User.self, from: data!)
-                        } else {
-                            print("data is nil")
-                        }
-                    } catch {
-                        
-                    }
-                }).resume()
-                if !user.email.isEmpty {
-                    print(user.email)
-                } else {
-                    print("no email")
-                }
-            } else {
-                print("url is nil")
+            if getData() {
+                performSegue(withIdentifier: "toMapView", sender: self)
             }
+        }
+    }
+    
+    func getData() -> Bool {
+        let id = "21232f297a57a5a743894a0e4a801fc3"
+        let urlStr = "http://baruchhaba.org/StudyBuddy/query.php?id=\(id)&type=select&table=user&condition=email%20=%20%22\(username.text!)%22"
+        if let url = URL(string: urlStr) {
+            var success = false
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                do {
+                    let decoder = JSONDecoder()
+                    if data != nil {
+                        self.user = try decoder.decode(User.self, from: data!)
+                        print("success!")
+                        print("Email: "+self.user.email)
+                        success = true
+                    }
+                } catch {
+                    
+                }
+            }).resume()
+            return success
+        } else {
+            return false
         }
     }
     
