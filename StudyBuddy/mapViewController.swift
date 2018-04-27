7f23e596f90af8686d1a76e2f7e3c96e482b4107
student <student@apc02hm0z4dv7n-7.local>
2018-04-27 18:32:37 -0400

    Local array of CramJams is now populated from the database.

--- StudyBuddy/mapViewController.swift
+++ StudyBuddy/mapViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 import MapKit
 import CoreLocation
 
+struct CramJam: Codable {
+    var host: String
+    var start_time: String
+    var end_time: String
+    var max_peeps: Int
+    var description: String
+}
+
 class mapViewController: UIViewController, CLLocationManagerDelegate{
     let locManager = CLLocationManager()
     
     var user: User = User()
+    
+    var cramJams: [CramJam] = []
     
     //map view that displays current CramJams and their locations
     @IBOutlet weak var cramJamMap: MKMapView!
@@ -33,8 +43,43 @@ class mapViewController: UIViewController, CLLocationManagerDelegate{
         centerMapOnLocation(location: curLocation, regionRadius: 500)
     }
     
+    func getData(table: String, condition: String, completion: @escaping (Bool)->()) {
+        let id = "21232f297a57a5a743894a0e4a801fc3"
+        var urlStr = "http://baruchhaba.org/StudyBuddy/query.php?id=\(id)&type=select&table=\(table)"
+        if !condition.isEmpty {
+            urlStr += "&condition=\(condition)"
+        }
+        if let url = URL(string: urlStr) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                do {
+                    let decoder = JSONDecoder()
+                    if data != nil {
+                        self.cramJams = try decoder.decode([CramJam].self, from: data!)
+                        completion(true)
+                    } else {
+                        completion(false)
+                    }
+                } catch {
+                    completion(false)
+                }
+            }).resume()
+        } else {
+            completion(false)
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        getData(table: "cram_jam", condition: "") { isValid in
+            DispatchQueue.main.async {
+                if isValid {
+                    // do something with cram jams, like update map with
+                } else {
+                    // give an error
+                }
+            }
+        }
         
         user = (presentingViewController as! ViewController).user
         (presentingViewController as! ViewController).user = User()
